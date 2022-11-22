bf0a988fd287a9c01bca69e0efce2b13dcbc33c5
Mohamed Khalil <105396865+mohamedkhalil-dev@users.noreply.github.com>
2022-11-22 19:07:12 +0300

    favicon update

--- main.py
+++ main.py
@@ -45,7 +45,7 @@ app.config['SECRET_KEY'] = os.environ.get("SECRET_KEY")
 ckeditor = CKEditor(app)
 Bootstrap(app)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DATABASE_URL")
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DATABASE_URL", "sqlite:///projects.db")
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
